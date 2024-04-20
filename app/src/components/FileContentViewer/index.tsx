8bcf52b1467f31b3d278d1523768b969702e57d0
Your Name <you@example.com>
2024-04-20 17:56:50 +0200

    previsualisation pdf

--- app/src/components/FileContentViewer/index.tsx
+++ app/src/components/FileContentViewer/index.tsx
@@ -3,6 +3,7 @@ import { FileType } from '../../types/File';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { Spin } from 'antd';
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 interface FileContentViewerProps {
@@ -14,10 +15,9 @@ const FileContentViewer: React.FC<FileContentViewerProps> = ({ blob, file }) => 
     const [url, setUrl] = useState<string>('');
     const [numPages, setNumPages] = useState<number>();
     const [pageNumber, setPageNumber] = useState<number>(1);
+    const [documentWidth, setDocumentWidth] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-        setNumPages(numPages);
-    }
 
     useEffect(() => {
         setUrl(URL.createObjectURL(blob));
@@ -26,6 +26,56 @@ const FileContentViewer: React.FC<FileContentViewerProps> = ({ blob, file }) => 
             URL.revokeObjectURL(url);
         };
     }, [blob]);
+
+    function waitForElementAndResize(selector: string, callback: (element: HTMLElement) => void) {
+        const observer = new MutationObserver((mutationsList) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    const element = document.querySelector(selector) as HTMLElement;
+                    if (element) {
+                        callback(element);
+                        observeResize(element);
+                        observer.disconnect(); // Arrêter d'observer une fois que l'élément est trouvé
+                        break;
+                    }
+                }
+            }
+        });
+    
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+    
+    function observeResize(element: HTMLElement) {
+        const resizeObserver = new ResizeObserver(() => {
+            console.log('Element resized');
+            console.log('Width:', element.clientWidth);
+    
+            // Mettre à jour le style de .ant-modal
+            const modal = document.querySelector('.ant-modal') as HTMLElement;
+            if (modal) {
+                const pdfWidth = element.clientWidth;
+                const newWidth = pdfWidth + 48;
+                modal.style.width = `${newWidth}px`;
+            }
+    
+            // Mettre à jour le style de .ant-modal-body
+            const modalBody = document.querySelector('.ant-modal-body') as HTMLElement;
+            if (modalBody) {
+                const pdfWidth = element.clientWidth;
+                const newWidth = pdfWidth + 48;
+                modalBody.style.width = `${newWidth}px`;
+            }
+        });
+    
+        resizeObserver.observe(element);
+    }
+    
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+        setNumPages(numPages);
+        waitForElementAndResize('.react-pdf__Page', (documentElement: HTMLElement) => {
+            console.log(documentElement.clientWidth);
+        });
+    }
 
     switch (file.extension?.toLowerCase()) {
         case 'jpg':
@@ -44,13 +94,17 @@ const FileContentViewer: React.FC<FileContentViewerProps> = ({ blob, file }) => 
 
         case 'pdf':
             return (
-                <div style={{ maxWidth: '100%', width: '100%', overflow: 'auto' }}>
-                    <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-                        {[...Array(numPages)].map((_, index) => (
-                            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                        ))}
-                    </Document>
-                </div>
+                <div id="pdf-document" style={{ maxWidth: '100%' }}>
+                    {loading ? (<Spin size="large" />) : (<></>)}
+
+                    <div id="pdf-document" style={{ maxWidth: '100%', display: loading ? 'none' : 'block' }}>
+                        <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+                            {[...Array(numPages)].map((_, index) => (
+                                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                            ))}
+                        </Document>
+                    </div>
+                </div >
             );
 
         case 'txt':
