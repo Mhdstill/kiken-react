7473dc1e0d5aaaf3c4cd07bca286a000f2b50ea2
Mehdi St <mehdist@MacBook-Pro.local>
2025-03-08 03:05:09 +0100

    debug visualiser file

--- app/src/components/Files/index.tsx
+++ app/src/components/Files/index.tsx
@@ -227,11 +227,6 @@ const FilesPage: FC<WithTranslation & WithDataManagerProps> = ({
         );
         if (file) {
           downloadFile(file);
-          const newSearchParams = new URLSearchParams(searchParams);
-          newSearchParams.delete('download');
-          newSearchParams.delete('id');
-          newSearchParams.delete('name');
-          window.history.replaceState({}, '', `${window.location.pathname}${newSearchParams.toString() ? '?' + newSearchParams.toString() : ''}`);
         }
       }
     },
@@ -239,7 +234,7 @@ const FilesPage: FC<WithTranslation & WithDataManagerProps> = ({
       console.error(e);
     },
     refetchOnWindowFocus: false,
-    refetchInterval: searchParams.has('download') ? false : 500,
+    refetchInterval: 500,
     refetchIntervalInBackground: true,
   });
 
